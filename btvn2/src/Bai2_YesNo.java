86dc6ee36d5301fe5efdd4773568c4d1df266422
kienle11 <letrungkien1611@gmail.com>
2023-07-24 15:24:34 +0700

    btvn2- java

---
+++ btvn2/src/Bai2_YesNo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Bai2_YesNo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Enter n:");
+        int n = Integer.parseInt(scanner.nextLine());
+        System.out.println("Do you want to continue? (Y/N)");
+        String answer = scanner.nextLine();
+        do {
+            if (answer.equals("y")) {
+                while (true) {
+                    System.out.println("Enter n:");
+                    n = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Do you want to continue? (Y/N)");
+                    answer = scanner.nextLine();
+                }
+            }
+            else{
+                for (int i = 0; i <= n; i++) {
+                    sum += i;
+                }
+                System.out.println(sum);
+                break;
+            }}
+            while (answer.equals("y") || answer.equals("n")) ;
+
+}
+}
+
+
