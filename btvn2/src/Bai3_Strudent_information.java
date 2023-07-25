5d24f7debaa2318046b4a9dc59c231a00f103584
kienle11 <letrungkien1611@gmail.com>
2023-07-25 18:49:44 +0700

    Update Bai3_Strudent_information.java

--- btvn2/src/Bai3_Strudent_information.java
+++ btvn2/src/Bai3_Strudent_information.java
@@ -3,34 +3,19 @@ import java.util.Scanner;
 public class Bai3_Strudent_information {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name = scanner.nextLine();
-        System.out.println("Enter your address:");
-        String address = scanner.nextLine();
-        System.out.println("Enter your age:");
-        int age = Integer.parseInt(scanner.nextLine());
-        System.out.println("Name: "+name);
-        System.out.println("Age: "+age);
-        System.out.println("Address: "+address);
-        System.out.println("Do you want to continue? (Y/N)");
-        String answer = scanner.nextLine();
+        String answer = "";
         do {
-            if (answer.equals("y")){
                 System.out.println("Enter your name:");
-                name = scanner.nextLine();
+                String name = scanner.nextLine();
                 System.out.println("Enter your address:");
-                address = scanner.nextLine();
+                String address = scanner.nextLine();
                 System.out.println("Enter your age:");
-                age = Integer.parseInt(scanner.nextLine());
+                int age = Integer.parseInt(scanner.nextLine());
                 System.out.println("Name: "+name);
                 System.out.println("Age: "+age);
                 System.out.println("Address: "+address);
                 System.out.println("Do you want to continue? (Y/N)");
                 answer= scanner.nextLine();
-            }
-            else {
-                break;
-            }
-        }while (answer.equals("y") || answer.equals("n"));
+        }while (answer.equals("y"));
     }
 }
