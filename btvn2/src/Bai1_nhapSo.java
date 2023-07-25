51b9cc83ad762b06341ab5c77f32726d21110d9c
kienle11 <letrungkien1611@gmail.com>
2023-07-25 10:50:05 +0700

    btvn2- java

---
+++ btvn2/src/Bai1_nhapSo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bai1_nhapSo {
+    public static void main(String[] args) {
+         /*  Viết chương trình cho phép nhập vào một số nguyên dương n, tính tổng tất cả số chẵn trong khoảng từ 0 - n.
+        Phuong phap Do-While*/
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Moi nhap so nguyen n:");
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        do {
+            if (n%2==0){
+                sum+=n;
+            }
+            n--;
+        }
+        while (n>0);
+
+        System.out.println("Tong cac so: "+sum);
+    }
+}
