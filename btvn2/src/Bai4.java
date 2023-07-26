6a6b332c7ccd1e111d7ace3e2bc597fa4fe5657e
kienle11 <letrungkien1611@gmail.com>
2023-07-27 01:13:45 +0700

    Update Bai4.java

--- btvn2/src/Bai4.java
+++ btvn2/src/Bai4.java
@@ -24,22 +24,34 @@ public class Bai4 {
             array1[n] = value;
             System.out.println("Mảng số mới sau khi thêm phần tử" + Arrays.toString(array1));
 
-            //Đổi chỗ phần tử 2-3 ( array1[2] - array1[3])
-            int y = array1[2];
+            //Cho người dùng thay đổi giá trị nhập:
+            String answer ="";
+            System.out.println("Ban co muốn sửa phần tử mới thêm vào? (Y/N)");
+            answer = scanner.nextLine();
+          while (answer.equals("y"))   {
+              System.out.println("Nhập phần tử mới");
+          value = Integer.parseInt(scanner.nextLine());
+          array1[n]=value;
+              System.out.println("Ban co muốn sửa phần tử mới thêm vào? (Y/N)");
+              answer = scanner.nextLine();
+          }
+            System.out.println(Arrays.toString(array1));
+
+          //Đổi chỗ phần tử 2-3 ( array1[2] - array1[3])
+            int temp1 = array1[2];
             array1[2] = array1[3];
-            array1[3] = y;
+            array1[3] = temp1;
             System.out.println("Mảng số mới sau khi đổi chỗ:" + Arrays.toString(array1));
 
             // Sắp xếp thứ tự mảng tăng dần
             for (int i = 0; i < n ; i++) {
                 for (int j = 0; j < n-i ; j++) {
                     if (array1[j + 1] < array1[j]) {
-                        int temp = array1[j ];
+                        int temp2 = array1[j ];
                         array1[j] = array1[j + 1];
-                        array1[j+1] = temp;
+                        array1[j+1] = temp2;
                     }
                 }
-
             }
             System.out.println("Mảng số sau khi sắp xếp tăng dần"+Arrays.toString(array1));
         }
