c6b60c1ddd3c21f9a6aec776ce5466f29defc72d
kienle11 <letrungkien1611@gmail.com>
2023-07-29 22:52:25 +0700

    Update Main.java

--- btvn-product/src/Main.java
+++ btvn-product/src/Main.java
@@ -1,17 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter product's ID:");
+        int id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter product's name:");
+        String name = scanner.nextLine();
+        System.out.println("Enter product's price");
+        double price = Double.parseDouble(scanner.nextLine());
+        Product product = new Product(name,id,price);
+        double vat = product.calculateVat();
+        System.out.println(product);
+        System.out.println("VAT: "+vat);
     }
 }
