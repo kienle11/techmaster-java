0541233481caed5f7bf87362cf926b0cab7beefc
kienle11 <letrungkien1611@gmail.com>
2023-07-29 23:17:37 +0700

    btvn2

---
+++ btvn2/src/Product/Btvn_Product.java
@@ -0,0 +1,19 @@
+package Product;
+
+import java.util.Scanner;
+
+public class Btvn_Product {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter product's ID:");
+        int id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter product's name:");
+        String name = scanner.nextLine();
+        System.out.println("Enter product's price");
+        double price = Double.parseDouble(scanner.nextLine());
+        Product product = new Product(name,id,price);
+        double vat = product.calculateVat();
+        System.out.println(product);
+        System.out.println("VAT: "+vat);
+    }
+}
