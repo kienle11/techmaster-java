fcf3d4e97449374d5949e6bee0bdb8c8f6359273
kienle11 <letrungkien1611@gmail.com>
2023-07-29 22:50:06 +0700

    btvn-product

---
+++ btvn-product/src/Product.java
@@ -0,0 +1,37 @@
+public class Product {
+    private int id;
+    private String name;
+
+    private double price;
+
+
+    public Product(String name, int id, double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+    public double calculateVat(){
+        return price*0.1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
