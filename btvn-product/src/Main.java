7297a39195b0407fae82a2a921e5e5bd45d424f2
kienle11 <letrungkien1611@gmail.com>
2023-07-29 22:55:37 +0700

    Update Main.java

--- btvn-product/src/Main.java
+++ btvn-product/src/Main.java
@@ -1,17 +1,6 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter product's ID:");
-        int id = Integer.parseInt(scanner.nextLine());
-        System.out.println("Enter product's name:");
-        String name = scanner.nextLine();
-        System.out.println("Enter product's price");
-        double price = Double.parseDouble(scanner.nextLine());
-        Product product = new Product(name,id,price);
-        double vat = product.calculateVat();
-        System.out.println(product);
-        System.out.println("VAT: "+vat);
+
     }
 }
