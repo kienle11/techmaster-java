847a0e940be12a145bd42f369b3b00b287dbf91e
kienle11 <letrungkien1611@gmail.com>
2023-08-17 11:40:21 +0700

    Update BorrowTicket.java

--- btvn2/src/techmaster/entities/BorrowTicket.java
+++ btvn2/src/techmaster/entities/BorrowTicket.java
@@ -3,6 +3,8 @@ package techmaster.entities;
 import java.time.LocalDate;
 
 public class BorrowTicket {
+    private static int autoID;
+    private int id;
     private Reader reader;
     private Book book;
     private LocalDate borrowDate = LocalDate.now();
@@ -12,6 +14,19 @@ public class BorrowTicket {
         this.reader = reader;
         this.book = book;
         this.returnDate = returnDate;
+        this.id = ++autoID;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "BorrowTicket" +id +
+                ". reader: " + reader +
+                ", book: " + book +
+                ", borrowDate: " + borrowDate +
+                ", returnDate: " + returnDate;
+    }
 }
